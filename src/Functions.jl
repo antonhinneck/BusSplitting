916e0cbfc84d3d52d2b0b62e195ded2407d509ed
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2020-02-13 13:52:20 +0300

    update

--- src/Functions.jl
+++ src/Functions.jl
@@ -1,6 +1,3 @@
-dv = 5
-W = Vector{Vector{Int8}}()
-
 function GenerateSplits!(W, dv; t = 0, Wv_last = Vector{Int8}())
     if sum(Wv_last) == dv
         push!(W, Wv_last)
@@ -24,9 +21,64 @@ function ω(Wv::Vector{Int8}, dv::Int8)
     return ω
 end
 
+function GenerateCombinations!(output, array, k::T where T <: Integer; t = 0, origin = Array{Int8, 1}(undef, k))
+
+    if t == k
+        push!(output, origin)
+    elseif t < k
+        for i in array
+            cnt = 1
+            if t == 0
+                new_array = copy(array)
+                deleteat!(new_array, cnt)
+                new_origin = copy(origin)
+                new_origin[t + 1] = i
+                GenerateCombinations!(output, new_array, k, t = t + 1, origin = new_origin)
+            else
+                if origin[t] <= i
+                    new_array = copy(array)
+                    deleteat!(new_array, cnt)
+                    new_origin = copy(origin)
+                    new_origin[t + 1] = i
+                    GenerateCombinations!(output, new_array, k, t = t + 1, origin = new_origin)
+                end
+            end
+            cnt += 1
+        end
+    end
+end
+
 #function assignments
 
+dv = 1
+W = Vector{Vector{Int8}}()
 GenerateSplits!(W, dv)
+W
+
+
+time_ns()
+ct = Vector{Float64}()
+delta = Vector{Int16}()
+permuts = Vector{Int16}()
+for i in 1:16
+    W = Vector{Vector{Int8}}()
+    start = time_ns()
+    GenerateSplits!(W, i)
+    elapsed = time_ns()
+    push!(permuts, length(W))
+    push!(ct, (elapsed - start)/10e9)
+    push!(delta, i)
+    println(i)
+end
+
+permuts
+
+output = Vector{Vector{Int8}}()
+
+array = [i for i in 1:dv]
+
+GenerateCombinations!(output, array, 3)
+output
 
 Ω = [ω(Wv, Int8(dv)) for Wv in W]
 
---
+++ src/plots.jl
@@ -0,0 +1,30 @@
+using PyPlot
+using Colors
+
+fig = figure(figsize=(8, 3))
+rc("font", family = "serif", style = "italic", size = 14)
+rc("text", usetex = true)
+rc("lines", linewidth = 1)
+
+ax = fig.add_axes([0.1,0.15,0.89,0.825])
+grid(linewidth = 0.2, linestyle = (0, (10, 10)), color = "lightgray")
+ax.tick_params(direction="in",top=true,right=true,width=1.4)
+
+#ax.set_axisbelow(true)
+ax.set_yscale("log")
+xlabel("\$\\delta_{v}\$")
+ylabel("\$t[s]\$")
+ylim(bottom = 10e-9, top = 10e0)
+##xlim(left=-5,right=5)
+#x = [0.01 * i for i in -50000:50000]
+
+for i in 1:length(permuts)
+    ax.annotate(string(permuts[i]), (delta[i] - 0.05, ct[i] * 0.16))
+end
+
+plot(delta, ct, color = "black", mec = "black", mfc = "white", label = "Split Generation", lw = 1.5, ls = "dotted", marker = "D", ms = 3.4, mew = 1.5)
+
+#plot(u_buses, pw(σ_vec), color = "black", mec = "black", mfc = "white", label = "\$\\sigma\$", lw = 1, ls = "dashed", marker = "o", ms = 2.4, mew = 1)
+
+legend(loc = "upper left", fancybox=false, edgecolor="black")
+savefig(string("computation_time.pdf"), format = :pdf)
